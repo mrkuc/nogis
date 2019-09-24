c185b12e7d2fa8837a256f300f83693f7f3485cc
mrkuc <2574472+mrkuc@users.noreply.github.com>
2019-09-25 05:11:38 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+const queries = process.argv.slice(2);
+
+(async() => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.co.jp/imghp?hl=ja');
+    await page.type("input[name='q']", queries.join(' '))
+    await Promise.all([
+        page.keyboard.press('Enter'),
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    ]);
+    
+    let es = await page.$$eval("div.rg_meta", es => {
+        return es.map(e => JSON.parse(e.textContent));
+    });
+
+    es.forEach(e => console.log(e.ou))
+    
+    browser.close();
+})().catch(err => console.log(err));
